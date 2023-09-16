94aac4948a8631df1dfabf8f84f6722dd65d7192
Syntax ð <alfrefp10001@gmail.com>
2023-09-16 14:42:40 -0500

    Path settings for files

--- api_service_nm/urls.py
+++ api_service_nm/urls.py
@@ -17,9 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from api.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('events/', get_all_events, name='event-list'),
     path('events/create/', create_event, name='create-event'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
